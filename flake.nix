1ccb9e0734bef94c3d3bf26d4284cc18ebd9470f
nortif8 <nortif8@gmail.com>
2024-03-04 10:09:55 -0700

    fmt

--- flake.nix
+++ flake.nix
@@ -4,16 +4,21 @@
   };
 
   outputs = { self, nixpkgs }:
-  let loadSys = sysName: {
-    system = "x86_64-linux";
-    modules = [
-      ./sys/${sysName}/config.nix
-      {virtualisation.vmVariant.virtualisation.cores = 4
-      ;virtualisation.vmVariant.virtualisation.memorySize = 8192
-      ;}
-    ];
-  };
-  in {
-	nixosConfigurations.orcus = nixpkgs.lib.nixosSystem (loadSys "orcus");
-  };
+    let
+      loadSys = sysName: {
+        system = "x86_64-linux";
+        modules = [
+          ./sys/${sysName}/config.nix
+          {
+            virtualisation.vmVariant.virtualisation.cores = 4
+            ;
+            virtualisation.vmVariant.virtualisation.memorySize = 8192
+            ;
+          }
+        ];
+      };
+    in
+    {
+      nixosConfigurations.orcus = nixpkgs.lib.nixosSystem (loadSys "orcus");
+    };
 }
